8d0cf918925c3d5e7ee5242f70d60e4abcc9dc7d
Soho <xiaohu@usf.edu>
2022-06-04 20:02:46 -0400

    understood

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -12,6 +12,28 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        # #iteratively
+        # prev, curr = None, head
+        # #> 1 - 2 - 3 - none
+        # while curr: # 1 (while loop)
+        #     temp = curr.next #save 2
+        #     curr.next = prev # 1 -> none
+        #     prev = curr # prev = 1
+        #     curr = temp # curr = 2
+        # return prev
+
+        ###################################
+        #recursively
+
+        if not head or not head.next:  #base case
+            return head # return 5
+            #curr head = 4
+        else:
+            newhead = self.reverseList(head.next) # assuming 5 #not pointer
+            head.next.next = head # 5 - > 4
+        head.next = None # 4 -> none
+
+        return newhead #what it passed is none 5... all the time.. finally undetstood
+
 # @lc code=end
 
